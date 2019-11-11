d5634507150ecc377050d3af6d70d805128a27e8
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-11 20:13:24 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,12 +42,13 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
   
-newArray = [ PORTION_1 ]
+newArray = { PORTION_1 }
   
 newArray[:sub_category][:sub_category][:sub_category] = PORTION_2
 
 newArray[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
 
 newArray[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+
 
 end
